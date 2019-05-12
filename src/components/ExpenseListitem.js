abb218a371f9af0e534dc4b5243cf29817cff7b2
Tamás Bogdán <born2shred92@gmail.com>
2019-05-12 19:55:50 +0200

    Fixed day format in moment-format

--- src/components/ExpenseListitem.js
+++ src/components/ExpenseListitem.js
@@ -8,7 +8,7 @@ export const ExpenseListitem = ({ dispatch, id, description, amount, createdAt }
         <Link className="list-item" to={`/edit/${ id }`} >
             <div>
                 <h3 className="list-item__title">{ description }</h3>
-                <span className="list-item__subtitle"> { moment(createdAt).format('do MMMM, YYYY') } </span>
+                <span className="list-item__subtitle"> { moment(createdAt).format('DD MMMM, YYYY') } </span>
             </div>
             <h3 className="list-item__data">{ numeral(amount / 100).format('$0.00') } </h3>
         </Link>
